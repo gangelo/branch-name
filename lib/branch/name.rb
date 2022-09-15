c96352be9765571ec94de923836a4863da2c6ac0
gangelo <web.gma@gmail.com>
2022-09-15 03:31:03 -0400

    Initial commit

---
+++ lib/branch/name.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "name/version"
+
+module Branch
+  module Name
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/branch/name/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Branch
+  module Name
+    VERSION = "0.1.0"
+  end
+end
