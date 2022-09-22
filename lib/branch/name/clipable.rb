99b9fdd9355a42ed42e95647b28fd173afc430ee
gangelo <web.gma@gmail.com>
2022-09-22 19:52:56 -0400

    Add support for windows clipboard and add specs

--- lib/branch/name/clipable.rb
+++ lib/branch/name/clipable.rb
@@ -1,15 +1,35 @@
 # frozen_string_literal: true
 
+require 'os'
+
 module Branch
   module Name
     module Clipable
       module_function
 
       def copy_to_clipboard(text)
-        if /darwin/.match?(RUBY_PLATFORM)
-          IO.popen('pbcopy', 'w') { |pipe| pipe.puts text }
+        if OS.mac?
+          copy_to_clipboard_macos text
           true
+        elsif OS.windows?
+          copy_to_clipboard_windows text
+          true
+        else
+          false
         end
+      end
+
+      def copy_to_clipboard_macos(text)
+        copy_to_clipboard_with(app: 'pbcopy', text: text)
+
+      end
+
+      def copy_to_clipboard_windows(text)
+        copy_to_clipboard_with(app: 'clip', text: text)
+      end
+
+      def copy_to_clipboard_with(app:, text:)
+        IO.popen(app, 'w') { |pipe| pipe.puts text }
       end
     end
   end
